fececc7886a419f05eb1e91ead592026fcf98918
Brad Searle <bradleysearle@gmail.com>
2023-08-03 09:48:03 +1000

    stub out cli

---
+++ src/cmd/cli.rs
@@ -0,0 +1,39 @@
+use std::fmt::Display;
+
+use clap::{Parser, Subcommand, ValueEnum};
+
+#[derive(Debug, Parser)] // requires `derive` feature
+#[command(name = "nk")]
+#[command(bin_name = "nk")]
+#[command(about = "Net Kraken, network connectivity tester.", long_about = None)]
+pub struct Cli {
+    /// Destination hostname or IP address
+    destination: String,
+
+    /// Destination port
+    port: u16,
+
+    #[clap(short, long, default_value_t = ConnectionMethod::Tcp)]
+    /// Connection Method
+    method: ConnectionMethod,
+}
+
+#[derive(ValueEnum, Clone, Debug, Default)]
+enum ConnectionMethod {
+    #[default]
+    Tcp,
+    Udp,
+    Icmp,
+    Http,
+}
+
+impl Display for ConnectionMethod {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ConnectionMethod::Tcp => write!(f, "tcp"),
+            ConnectionMethod::Udp => write!(f, "udp"),
+            ConnectionMethod::Icmp => write!(f, "icmp"),
+            ConnectionMethod::Http => write!(f, "http"),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,66 @@
+mod cmd;
+mod tcp;
+
 use std::io;
-use tokio::net::{TcpStream, UdpSocket};
+use std::net::SocketAddr;
+
+use clap::Parser;
+use tokio::io::Interest;
+use tokio::net::{TcpSocket, TcpStream, UdpSocket};
+
+use crate::cmd::cli::Cli;
+use crate::tcp::client::TcpConnector;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let t = TcpStream::connect("172.31.255.20:3000").await?;
-    println!("{:#?}", t);
+    let t_client = TcpConnector::new("172.31.255.21", 3002);
+    println!("{:#?}", t_client);
+
+    let bind_addr = SocketAddr::from(([192, 168, 255, 238], 0));
+    // let bind_addr = SocketAddr::from(([172, 24, 255, 59], 0));
+    let connect_addr = SocketAddr::from(([172, 31, 255, 20], 3000));
+
+    let socket = TcpSocket::new_v4()?;
+    socket.bind(bind_addr)?;
+
+    let stream = socket.connect(connect_addr).await?;
+
+    println!("{:#?}", stream);
+    println!("{:#?}", stream.local_addr());
+
+    // match socket.connect(connect_addr).await {
+    //     Ok(_) => println!("connected"),
+    //     Err(e) => match e.kind() {
+    //         io::ErrorKind::ConnectionRefused => println!("connection refused"),
+    //         io::ErrorKind::TimedOut => println!("connection timed out"),
+    //         _ => println!("{:#?}", e),
+    //     },
+    // }
+
+    // println!("{:#?}", socket.local_addr());
+
+    // println!("{:#?}", socket);
+
+    // match TcpStream::connect(format!("{}:{}", t_client.dst_ip, t_client.dst_port)).await {
+    //     Ok(_) => println!("connected"),
+    //     Err(e) => match e.kind() {
+    //         io::ErrorKind::ConnectionRefused => println!("connection refused"),
+    //         io::ErrorKind::TimedOut => println!("connection timed out"),
+    //         _ => println!("{:#?}", e),
+    //     },
+    // };
+
+    // println!("{:#?}", t);
+    // println!("{:#?}", t.local_addr());
+    // println!("{:#?}", t.linger());
+    // println!("{:#?}", t.ttl());
+
+    // let ready = t.ready(Interest::READABLE | Interest::WRITABLE).await?;
+    // println!("{:#?}", ready);
+
+    // if ready.is_writable() {
+    //     println!("HELLO")
+    // }
 
     // Bind socket
     let socket = UdpSocket::bind("192.168.255.238:0").await?;
@@ -12,6 +68,9 @@ async fn main() -> io::Result<()> {
 
     // Send a message
     socket.send(b"hello world\n").await?;
+
+    let args = Cli::parse();
+    println!("{:#?}", args);
 
     Ok(())
 }
