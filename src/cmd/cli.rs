dd9ba329eb80ca0da94252cbf6d4e9b274a103f0
Brad Searle <bradleysearle@gmail.com>
2023-08-03 17:29:44 +1000

    clean up udp

--- src/cmd/cli.rs
+++ src/cmd/cli.rs
@@ -1,6 +1,12 @@
 use std::fmt::Display;
+use std::io;
+use std::net::SocketAddr;
 
+use anyhow::Result;
 use clap::{Parser, ValueEnum};
+use tokio::net::{TcpSocket, UdpSocket};
+
+use crate::util::parser::parse_ipaddr;
 
 #[derive(Debug, Parser)] // requires `derive` feature
 #[command(name = "nk")]
@@ -44,4 +50,47 @@ impl Display for ConnectionMethod {
             ConnectionMethod::Http => write!(f, "http"),
         }
     }
+}
+
+pub async fn init_cli() -> Result<()> {
+    let cli = Cli::parse();
+
+    let src_addr = parse_ipaddr(&cli.src_addr)?;
+    let dst_addr = parse_ipaddr(&cli.dst_host)?;
+
+    let bind_addr = SocketAddr::new(src_addr, cli.src_port);
+    let connect_addr = SocketAddr::new(dst_addr, cli.dst_port);
+
+    match cli.method {
+        ConnectionMethod::Http => println!("http not implemented"),
+        ConnectionMethod::Icmp => println!("icmp not implemented"),
+        ConnectionMethod::Tcp => {
+            let socket = match src_addr.is_ipv4() {
+                true => TcpSocket::new_v4()?,
+                false => TcpSocket::new_v6()?,
+            };
+
+            socket.bind(bind_addr)?;
+
+            match socket.connect(connect_addr).await {
+                Ok(s) => {
+                    println!("connected to {} using {}", s.peer_addr()?, cli.method);
+                }
+                Err(e) => match e.kind() {
+                    io::ErrorKind::ConnectionRefused => println!("connection refused"),
+                    io::ErrorKind::TimedOut => println!("connection timed out"),
+                    _ => println!("{:#?}", e),
+                },
+            };
+        }
+        ConnectionMethod::Udp => {
+            let dst_ip_port_str = format!("{}:{}", dst_addr, cli.dst_port);
+            let conn_string = format!("connection from: {}\n", bind_addr);
+
+            let socket = UdpSocket::bind(bind_addr).await?;
+            socket.connect(dst_ip_port_str).await?;
+            socket.send(conn_string.as_bytes()).await?;
+        }
+    }
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,10 @@ mod cmd;
 mod tcp;
 mod util;
 
-use std::io;
-use std::net::SocketAddr;
-
-use clap::Parser;
-use tokio::net::{TcpSocket, UdpSocket};
-
-use crate::cmd::cli::{Cli, ConnectionMethod};
-use crate::util::parser::parse_ipaddr;
+use crate::cmd::cli::init_cli;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let cli = Cli::parse();
-
-    let src_addr = parse_ipaddr(&cli.src_addr)?;
-    let dst_addr = parse_ipaddr(&cli.dst_host)?;
-
-    let bind_addr = SocketAddr::new(src_addr, cli.src_port);
-    let connect_addr = SocketAddr::new(dst_addr, cli.dst_port);
-
-    match cli.method {
-        ConnectionMethod::Http => println!("http not implemented"),
-        ConnectionMethod::Icmp => println!("icmp not implemented"),
-        ConnectionMethod::Tcp => {
-            let socket = match src_addr.is_ipv4() {
-                true => TcpSocket::new_v4()?,
-                false => TcpSocket::new_v6()?,
-            };
-
-            socket.bind(bind_addr)?;
-
-            match socket.connect(connect_addr).await {
-                Ok(s) => {
-                    println!("connected to {} using {}", s.peer_addr()?, cli.method);
-                }
-                Err(e) => match e.kind() {
-                    io::ErrorKind::ConnectionRefused => println!("connection refused"),
-                    io::ErrorKind::TimedOut => println!("connection timed out"),
-                    _ => println!("{:#?}", e),
-                },
-            };
-        }
-        ConnectionMethod::Udp => {
-            let socket = UdpSocket::bind("0.0.0.0:0").await?;
-            socket.connect("172.31.255.20:3001").await?;
-            let conn_string = format!("connection from: {}\n", socket.local_addr().unwrap());
-            socket.send(conn_string.as_bytes()).await?;
-            println!("{:#?}", socket.local_addr())
-        }
-    }
-
+    init_cli().await?;
     Ok(())
 }
