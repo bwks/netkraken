28ea3332eb1feac760feb1fe9a85ecc55bfbacce
Brad Searle <bradleysearle@gmail.com>
2023-08-19 09:21:38 +1000

    testing 123

--- src/util/handler.rs
+++ src/util/handler.rs
@@ -6,14 +6,24 @@ use crate::core::common::LogLevel;
 use crate::core::common::OutputOptions;
 use crate::core::konst::APP_NAME;
 
-pub async fn loop_handler(count: u16, repeat: u16, sleep_interval: u16) -> bool {
-    if count == u16::MAX {
+/// Handler to manage loop iterations. On `true` the loop
+/// will break, on `false` it will continue.
+/// # Arguments
+/// * `loop_counter` - The loop iteration count
+/// * `num_repeats` - How many times the loop should num_repeats
+/// * `sleep_interval` - How long the loop should sleep for between iterations
+///  
+/// ## Break conditions
+///  * loop count == 65535 (u16 max value)
+///  * loop count >= number of repeats
+pub async fn loop_handler(loop_count: u16, num_repeats: u16, sleep_interval: u16) -> bool {
+    if loop_count == u16::MAX {
         println!("max ping count reached");
         return true;
-    } else if repeat != 0 && count >= repeat {
+    } else if num_repeats != 0 && loop_count >= num_repeats {
         return true;
     } else {
-        if count > 0 {
+        if loop_count > 0 {
             sleep(Duration::from_millis(sleep_interval.into())).await;
         }
         return false;
@@ -35,5 +45,34 @@ pub async fn output_handler(log_level: LogLevel, message: &String, output_option
     }
     if output_options.json {
         // json handler
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::util::handler::*;
+
+    #[tokio::test]
+    async fn loop_handler_with_max_count_is_true() {
+        let result = loop_handler(65535, 0, 1).await;
+        assert_eq!(result, true);
+    }
+
+    #[tokio::test]
+    async fn loop_handler_with_loop_count_gt_num_repeats_is_true() {
+        let result = loop_handler(2, 1, 1).await;
+        assert_eq!(result, true);
+    }
+
+    #[tokio::test]
+    async fn loop_handler_with_loop_count_eq_num_repeats_is_true() {
+        let result = loop_handler(1, 1, 1).await;
+        assert_eq!(result, true);
+    }
+
+    #[tokio::test]
+    async fn loop_handler_with_repeat_count_gt_0_is_false() {
+        let result = loop_handler(0, 1, 1).await;
+        assert_eq!(result, false);
     }
 }
