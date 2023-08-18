c604b1f4a7fbd382afff8e5b40c31f973c10358d
Brad Searle <bradleysearle@gmail.com>
2023-08-18 20:25:03 +1000

    tests

--- src/util/parser.rs
+++ src/util/parser.rs
@@ -4,6 +4,7 @@ use anyhow::{bail, Result};
 
 use crate::core::common::NetKrakenMessage;
 
+/// Parse into a std::net::IPv4 or std::net::IPv6 address from a string
 pub fn parse_ipaddr(s: &String) -> Result<IpAddr> {
     if !s.contains(".") && !s.contains(":") {
         bail!("source address: `{s}` is invalid")
@@ -16,15 +17,54 @@ pub fn parse_ipaddr(s: &String) -> Result<IpAddr> {
 }
 
 /// Attempt to read in a NetKrakenMessage from a string
-/// If the string be read into a NetKrakenMessage then
+/// If the string cannot be read into a NetKrakenMessage then
 /// it will be assumed that the peer is not a NetKraken host
 pub fn nk_msg_reader(s: &str) -> Option<NetKrakenMessage> {
     let data: NetKrakenMessage = match serde_json::from_str(s) {
-        // If we can read this hello message we have a
-        // NetKraken Peer
+        // If we can read this message we have a NetKraken Peer
         Ok(d) => d,
         // Not a NetKraken peer
         Err(_) => return None,
     };
     Some(data)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::{Ipv4Addr, Ipv6Addr};
+
+    use crate::core::common::NetKrakenMessage;
+    use crate::util::parser::{nk_msg_reader, parse_ipaddr};
+
+    const IPV4_ADDR: &str = "198.51.100.1";
+    const IPV6_ADDR: &str = "2001:0DB8::1";
+
+    #[test]
+    fn parse_ipaddr_with_ipv4_addr() {
+        let result = parse_ipaddr(&IPV4_ADDR.to_owned()).unwrap();
+        assert_eq!(result, Ipv4Addr::new(198, 51, 100, 1));
+    }
+
+    #[test]
+    fn parse_ipaddr_with_ipv6_addr() {
+        let result = parse_ipaddr(&IPV6_ADDR.to_owned()).unwrap();
+        assert_eq!(result, Ipv6Addr::new(0x2001, 0x0DB8, 0, 0, 0, 0, 0, 1));
+    }
+
+    #[test]
+    #[should_panic]
+    fn parse_ipaddr_with_invalid_param() {
+        parse_ipaddr(&"blah".to_string()).unwrap();
+    }
+
+    #[test]
+    fn parse_nk_message_some() {
+        let msg = serde_json::to_string(&NetKrakenMessage::default()).unwrap();
+        assert!(nk_msg_reader(&msg).is_some());
+    }
+
+    #[test]
+    fn parse_nk_message_none() {
+        assert!(nk_msg_reader("msg").is_none());
+    }
 }
--- src/util/time.rs
+++ src/util/time.rs
@@ -10,20 +10,70 @@ pub fn time_now_us() -> Result<u128> {
     Ok(since_the_epoch.as_micros())
 }
 
-/// Get the current date and time in UTC
-#[allow(dead_code)]
+/// Get the current UTC date and time as a string
 pub fn time_now_utc() -> String {
     let time_now = OffsetDateTime::now_utc();
     time_now.to_string()
 }
 
+/// Calculate the amount of time for a connection
+/// pre_timestamp and post_timestamp are unix timestamps in (u) microseconds
+/// a float value is returned represented as milliseconds
 pub fn calc_connect_ms(pre_timestamp: u128, post_timestamp: u128) -> f64 {
-    match post_timestamp < pre_timestamp {
+    match (post_timestamp < pre_timestamp) || (pre_timestamp < 1000) || (post_timestamp < 1000) {
         // clocks are not sufficiently synced to calculate difference
         true => -1.0,
         false => {
             let us = post_timestamp - pre_timestamp;
             us as f64 / 1000.0
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::util::time::calc_connect_ms;
+
+    #[test]
+    fn calc_connect_ms_returns_1ms() {
+        let pre_timestamp = 1000;
+        let post_timestamp = 2000;
+        let result = calc_connect_ms(pre_timestamp, post_timestamp);
+        assert_eq!(result, 1.0);
+    }
+    #[test]
+    fn calc_connect_ms_returns_neg_1ms() {
+        let pre_timestamp = 2000;
+        let post_timestamp = 1000;
+        let result = calc_connect_ms(pre_timestamp, post_timestamp);
+        assert_eq!(result, -1.0);
+    }
+    #[test]
+    fn calc_connect_ms_returns_0ms() {
+        let pre_timestamp = 1000;
+        let post_timestamp = 1000;
+        let result = calc_connect_ms(pre_timestamp, post_timestamp);
+        assert_eq!(result, 0.0);
+    }
+    #[test]
+    fn calc_connect_ms_returns_9_877ms() {
+        let pre_timestamp = 10123;
+        let post_timestamp = 20000;
+        let result = calc_connect_ms(pre_timestamp, post_timestamp);
+        assert_eq!(result, 9.877);
+    }
+    #[test]
+    fn calc_connect_ms_with_pre_timestamp_neg_1000_returns_1ms() {
+        let pre_timestamp = 900;
+        let post_timestamp = 1000;
+        let result = calc_connect_ms(pre_timestamp, post_timestamp);
+        assert_eq!(result, -1.0);
+    }
+    #[test]
+    fn calc_connect_ms_with_post_timestamp_neg_1000_returns_1ms() {
+        let pre_timestamp = 1000;
+        let post_timestamp = 900;
+        let result = calc_connect_ms(pre_timestamp, post_timestamp);
+        assert_eq!(result, -1.0);
     }
 }
